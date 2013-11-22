61edcaac79485f02e0d7b05cb2af934163eb63fd
Alain Malenfant <alainmalenfant@yahoo.com>
2013-11-22 10:21:09 -0500

    Initial Commit

---
+++ MyLocations/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  MyLocations
+//
+//  Created by Alain Malenfant on 11/22/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ MyLocations/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  MyLocations
+//
+//  Created by Alain Malenfant on 11/22/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
